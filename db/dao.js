50c18defa9502cc5a58c3fde7d681c01f6ed5ddb
Burak FIRAT <burakfirat@pm.me>
2021-12-09 23:18:02 +0300

    Added entities(user, product, order) Aded data access object

--- db/dao.js
+++ db/dao.js
@@ -5,13 +5,24 @@ class Database {
         this.connection = connection;
     }
 
-    query(sql, args) {
+    run(sql, args) {
+        //THIS FUNCTION FOR INSERT, UPDATE, DELETE
+        //lastID for successful insert process
+        //changes for successful update, delete processes
         return new Promise((resolve, reject) => {
-            this.connection.run(sql, args, (err, rows) => {
-                if (err)
-                    return reject(err);
-                resolve(rows);
+            let statement = this.connection.prepare(sql, args).run((err) => {
+                if (err) return reject(err);
+                resolve({"changes": statement.changes, "lastID": statement.lastID});
             });
+        });
+    }
+
+    get(sql, args) {
+        return new Promise((resolve, reject) => {
+            this.connection.get(sql, args, (err, row) => {
+                console.log(row);
+                resolve(row);
+            })
         });
     }
 
