45c4a66d6028ef51db66428143f4e003a14e67c5
Burak FIRAT <burakfirat@pm.me>
2021-12-09 23:18:51 +0300

    Added entity files

---
+++ entity/product.js
@@ -0,0 +1,14 @@
+class Product {
+    constructor(product) {
+        this.id = product.id;
+        this.name = product.name;
+        this.description = product.description;
+        this.price = product.price;
+        this.stock = product.stock;
+        this.created_at = product.created_at;
+        this.modified_at = product.modified_at;
+        this.deleted_at = product.deleted_at;
+    }
+}
+
+module.exports = Product;
