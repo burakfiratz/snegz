b7aff7ea75d6fd01a37e18e4d7ffdd37d77b00bf
Burak FIRAT <burakf@shopier.com>
2021-12-09 23:18:02 +0300

    Added entities(user, product, order) Aded data access object

--- schema.js
+++ schema.js
@@ -2,18 +2,21 @@ const {GraphQLSchema, GraphQLObjectType, GraphQLInt, GraphQLString, GraphQLFloat
 
 const connection = require('./db/dao');
 
+const User = require("./entity/user");
+const Product = require("./entity/product");
+
 let userType = new GraphQLObjectType({
     name: 'User',
-    fields: {
+    fields: () => ({
         id: {type: GraphQLInt},
         username: {type: GraphQLString},
         email: {type: GraphQLString}
-    }
+    })
 });
 
 let productType = new GraphQLObjectType({
     name: 'Product',
-    fields: {
+    fields: () => ({
         id: {type: GraphQLInt},
         name: {type: GraphQLString},
         description: {type: GraphQLString},
@@ -22,7 +25,7 @@ let productType = new GraphQLObjectType({
         created_at: {type: GraphQLString},
         modified_at: {type: GraphQLString},
         deleted_at: {type: GraphQLString},
-    }
+    })
 });
 
 
@@ -40,17 +43,29 @@ var queryType = new GraphQLObjectType({
             args: {
                 id: {type: GraphQLInt}
             },
-            resolve: async (_) => {
-                //TODO: sqlite db query
+            resolve: async (_, args) => {
+                return await connection.get('SELECT * FROM users WHERE id=?', [args.id]).then(async result => {
+                    return new User(result);
+                });
             }
         },
+        getProduct: {
+            type: productType,
+            args: {
+                id: {type: GraphQLInt}
+            },
+            resolve: async (_, args) => {
+                return await connection.get('SELECT * FROM products WHERE id=?', [args.id]).then(async result => {
+                    return new Product(result)
+                });
+            }
+        }
     }
 });
 
 var mutationType = new GraphQLObjectType({
     name: 'Mutation',
     fields: () => ({
-
         setUser: {
             type: userType,
             args: {
@@ -58,7 +73,14 @@ var mutationType = new GraphQLObjectType({
                 email: {type: GraphQLString}
             },
             resolve: async (_, args) => {
-                await connection.query('INSERT INTO users (username, email) VALUES (?,?)', [args.username, args.email]);
+                return await connection.run('INSERT INTO users (username, email) VALUES (?,?)', [args.username, args.email]).then(async result => {
+                    console.log(result.lastID);
+                    return await connection.get('SELECT * FROM users WHERE id=?', [result.lastID]).then(result => {
+                        console.log(result);
+                        console.log(new User(result));
+                        return new User(result);
+                    });
+                });
             }
         },
         setProduct: {
@@ -71,8 +93,13 @@ var mutationType = new GraphQLObjectType({
                 stock: {type: GraphQLInt}
             },
             resolve: async (_, args) => {
-                await connection.query('INSERT INTO products (user_id, name, description, price, stock) VALUES (?,?,?,?,?)',
-                    [args.user_id, args.name, args.description, args.price, args.stock]);
+                return await connection.run('INSERT INTO products (user_id, name, description, price, stock) VALUES (?,?,?,?,?)',
+                    [args.user_id, args.name, args.description, args.price, args.stock]).then(async result => {
+                    console.log(result.lastID);
+                    return await connection.get('SELECT * FROM products WHERE id=?', [result.lastID]).then(result => {
+                        return new Product(result)
+                    });
+                });
             }
         }
     })
